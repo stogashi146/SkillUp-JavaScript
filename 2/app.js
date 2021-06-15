2dd0bf47397bab00db3e2439dee94a1872139549
富樫駿 <togashishun@shunnoMacBook-Air.local>
2021-06-15 19:55:49 +0900

    [Finish] js-2

---
+++ 2/app.js
@@ -0,0 +1,39 @@
+// var hello = 'Hello World'
+// alert(hello);
+
+// alert(4+3);
+
+// alert("hello"+"World");
+
+// var orange = 100;
+// var apple = 120;
+
+// // IF文
+// if(orange < apple){
+//   alert("みかんの値段がりんごより安い");
+// }else if(orange == apple){
+//   "みかんとりんごが同じ値段"
+// }else{
+//   alert("みかんの値段がりんごより高い")
+// }
+
+// WHILE文
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1
+// }
+// alert("2を掛け続けて" + max + "を超えるのに必要だった回数は"+count +"回です")
+
+// for文
+var i;
+var num = 0;
+for(i=1;i<11;i++)
+{
+  num = num + i;
+}
+
+alert("1から10まで足し算した結果は"+ num +　"です");
