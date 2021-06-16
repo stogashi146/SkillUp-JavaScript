34a403e341d01bbf802b0cb8dd4ef98ddd26ae0b
富樫駿 <togashishun@shunnoMacBook-Air.local>
2021-06-16 10:35:32 +0900

    [Finish] js-4

---
+++ 2/Challenge/app.js
@@ -0,0 +1,15 @@
+var a;
+var b;
+
+
+var user_a = prompt("値1を入力");
+var user_b = prompt("値1を入力");
+operator(user_a,user_b);
+alert(a1);
+
+function operator(a,b){
+  a + b == a1;
+  a - b==a2;
+  a * b==a3;
+  a / b==a4;
+}
