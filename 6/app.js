c4aba836c2c42ff63b4c345a7181500282ebe94b
富樫駿 <togashishun@shunnoMacBook-Air.local>
2021-06-16 11:56:10 +0900

    [Finish] js-6

---
+++ 6/app.js
@@ -0,0 +1,32 @@
+// マウスオーバ、アウトで色変更
+// $(function(){
+//   $(".box1").mouseover(function(){
+//     $(".box1").css({"background-color":"#0000ff"});
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").css({"background-color":"#ff0000"});
+//   });
+// });
+
+// マウスオーバ、アウトでクラス変更
+// $(function(){
+//   $(".box1").mouseover(
+//     function(){
+//       $(".box1").addClass("box1-ext");
+//     });
+//   $(".box1").mouseout(
+//     function(){
+//       $(".box1").removeClass("box1-ext");
+//     })
+// })
+
+// マウスクリックでクラス変更
+$(function(){
+  $(".box1").on("click",function(){
+    $(".box1").addClass("box1-ext");
+  });
+  $(".box1").mouseout(function(){
+    $(".box1").removeClass("box1-ext");
+  })
+}
+  )
