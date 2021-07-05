1691b555b55f987b6476d686e686b2e4aab77946
2DPuzzle <stogashi146@gmail.com>
2021-07-05 16:56:00 +0900

    [Finish]JS-4

---
+++ review/2/app.js
@@ -0,0 +1,10 @@
+// let hello = "Hello World"
+// alert(hello);
+
+let int1 = 1;
+let int2 = 10;
+
+alert(int1 + int2);
+alert(int1 - int2);
+alert(int1 * int2);
+alert(int1 / int2);
---
+++ review/4/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("body").html("<h1>Hello jQuery!!</h1>")
+});
+$(function(){
+  
+})
