b4d705e3b790541d93d62600f3b445927a3a1686
stogashi146 <stogashi146@gmail.com>
2021-10-13 21:36:09 +0900

    [Finish]３章じゃんけん関数

---
+++ 202110/3/app.js
@@ -0,0 +1,84 @@
+// 関数を出力する
+// let alertString;
+// alertString = addString("webcamp");
+//
+// alert(alertString);
+//
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+// 入力した文字列を出力する
+// let user_hand = prompt("じゃんけんの手をグー、チョキ、パーから選んでください。");
+// alert("あなたの選んだ手は" + user_hand + "です");
+
+// じゃんけんの関数
+
+let user_hand = prompt("じゃんけんの手をグーチョキパーから選んでください")
+let js_hand = getJShand();
+let judge = winLose(user_hand, js_hand);
+alert(handCheck(user_hand));
+
+function handCheck(hand){
+  let result;
+  if(hand == null){
+    result = "またチャレンジしてね";
+  }else if(user_hand != "グー" || user_hand != "チョキ" || user_hand != "パー"){
+    result = "グーチョキパーのいずれかを入力してください。";
+    user_hand = prompt("じゃんけんの手をグーチョキパーから選んでください")
+  }else{
+    result = "あなたの選ん手は" + user_hand + "です。\nJavaScriptの選んだ手は" + js_hand + "です。結果は" + judge + "です";
+  }
+  return result
+}
+
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random()*3);
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  }else if(js_hand_num == 1){
+    hand_name = "チョキ"
+  }else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+  return hand_name;
+}
+
+function winLose(user,js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    }else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    }else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  }else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    }else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    }else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  }else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    }else if(js == "チョキ"){
+      winLoseStr = "負け";
+    }else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }else if(user == null){
+    winLoseStr = "またチャレンジしてね";
+  }else{
+    winLoseStr = "グーチョキパーのいずれかを入力してください。";
+    return;
+  }
+  return winLoseStr;
+}
