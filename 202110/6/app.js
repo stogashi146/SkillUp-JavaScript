996e55c16cc99456be135bfe9f432be85fd9a859
stogashi146 <stogashi146@gmail.com>
2021-10-15 16:13:10 +0900

    [Finish]Chapter7

---
+++ 202110/6/app.js
@@ -0,0 +1,32 @@
+// 正方形をスライドアップさせる
+// $(function(){
+//   $(".bg1").on("click",function(){
+//     $(".bg1").slideUp();
+//   });
+//
+//   $(".bg2").on("click",function(){
+//     $(".bg2").slideUp();
+//   });
+//
+//   $(".bg3").on("click",function(){
+//     $(".bg3").slideUp();
+//   });
+//
+//   $(".bg4").on("click",function(){
+//     $(".bg4").slideUp();
+//   });
+// })
+
+// 省略した書き方
+// thisでクリックした要素を判別する
+// $(function(){
+//   $(".box1").on("click",function(){
+//     $(this).slideUp();
+//   });
+// });
+
+$(function(){
+  $("button").on("click",function(){
+    $("ul").children().css("color","red");
+  });
+});
